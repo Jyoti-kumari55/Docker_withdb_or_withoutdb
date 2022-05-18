4182fa6553fcb4acd28f0ccf8ee48ea5a29396e2
unknown <Jyotichaudhary6500@gmail.com>
2022-05-18 23:32:02 +0530

    first commit

---
+++ Spring_JPA/src/test/java/com/example/jpa/SpringJpaApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.jpa;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaApplicationTests {
+
+	// @Test
+	void contextLoads() {
+	}
+
+}
